94a0ccf0065f8c2e98ebed58a1c92f9d39c83e7f
denver1117 <emitchellh@gmail.com>
2017-10-30 16:42:29 +0000

    Add drop files

---
+++ build/drop_files.sh
@@ -0,0 +1,5 @@
+# remove all temporary files
+sudo rm -rf /var/www/html/classificator/tmp/*
+sudo rm -rf /tmp/classificator/*
+sudo rm -rf /var/www/html/classificator/logs/*
+sudo rm -rf /var/www/html/classificator/configs/*
